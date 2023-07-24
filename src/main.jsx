19b6012eb827209899b3b1467a045d1347667e05
Michaellara12 <michaellara012@gmail.com>
2023-07-24 11:22:49 -0500

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import Calentadores from './routes/calentadores.jsx';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/calentadores',
+    element: <Calentadores />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/routes/calentadores.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from 'react'
+import '../App.css'
+
+function Calentadores() {
+  
+  const randomValue = Math.random();
+
+  useEffect(() => {
+     // Get a random number between 0 and 1
+
+    // Define the two URLs you want to split the traffic between
+    const url1 = 'https://api.whatsapp.com/send?phone=573118823072&text=Hola%20grupo%20Ecoled,%20quisiera%20saber%20m%C3%A1s%20sobre%20sus%20calentadores%20solares';
+    const url2 = 'https://api.whatsapp.com/send?phone=573132623425&text=Hola%20grupo%20Ecoled,%20quisiera%20saber%20m%C3%A1s%20sobre%20sus%20calentadores%20solares';
+
+    // Check if the random value is less than 0.5 (50% probability)
+    // and redirect accordingly
+    if (randomValue < 0.5) {
+      window.location.href = url1;
+    } else {
+      window.location.href = url2;
+    }
+  }, [])
+
+  return (
+    <>
+      <div>
+          <img className="logo" alt="Vite logo" src="/logo.png" />
+      </div>
+      <h1>Grupo Ecoled Colombia</h1>
+      <div className="card">
+        <p>
+          Te estamos conectando con un agente...
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Calentadores
